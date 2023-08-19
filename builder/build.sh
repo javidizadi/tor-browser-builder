21e72ef684c4edb2d668f35bd2986feb9ac1ba48
Javid izadi <javid.izadi1384@gmail.com>
2023-08-19 17:25:55 +0330

    fix build script

--- builder/build.sh
+++ builder/build.sh
@@ -20,11 +20,12 @@ prepare_source() {
 build() {
 	local source_path=$1
 	local enviroment_vars=$2
-	if [[ -z "$enviroment_vars" ]]; then
-		enviroment_vars=" "
-	fi
 	cd "$source_path" || exit 1
-	/bin/env "$enviroment_vars" makepkg -sc --noconfirm --noprogressbar
+	if [[ -z "$enviroment_vars" ]]; then
+		makepkg -sc --noconfirm --noprogressbar
+	else
+		env "$enviroment_vars" makepkg -sc --noconfirm --noprogressbar
+	fi
 }
 generate_version() {
 	local PKGBUILD_path=$1
